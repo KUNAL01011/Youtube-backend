56ba43fb1f7d1259bc3b6cc337ac75e0b6470558
Kunal Kumar <kunal34255@gmail.com>
2023-12-29 08:49:18 +0530

    Here we carete a middleware and cloudinary setup

---
+++ src/utils/cloudinary.js
@@ -0,0 +1,34 @@
+import { v2 as cloudinary } from "cloudinary";
+import fs from "fs";
+
+
+// cloudinary config file          
+cloudinary.config({ 
+  cloud_name: process.env.CLOUDINARY_CLOUD_NAME, 
+  api_key: process.env.CLOUDINARY_API_KEY, 
+  api_secret: process.env.CLOUDINARY_API_SECRET  
+});
+
+
+
+const uploadOnCloudinary = async (localFilePath) => {
+    try {
+        if(!localFilePath){
+            return null
+        }
+        //upload the file on cloudinary
+        const response = await cloudinary.uploader.upload(localFilePath, {
+            resource_type: "auto"
+        })
+
+        //file has been uploaded successfully
+        console.log("file is uploaded on cludinary",response.url);
+        return response
+    } catch (error) {
+        fs.unlinkSync(localFilePath)//remove the locally saved temparay file as the upload operation go failed
+        return null;
+
+    }
+}
+
+export {uploadOnCloudinary};
