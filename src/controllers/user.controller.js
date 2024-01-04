bdad4eb5191d4868f09082e60e9bf7fc74a22083
Kunal Kumar <kunal34255@gmail.com>
2024-01-04 08:08:47 +0530

    commenting

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -1,4 +1,4 @@
-// import {asyncHandler } from "../utils/asyncHandler.js";
+/*// import {asyncHandler } from "../utils/asyncHandler.js";
 // import {ApiError} from "../utils/ApiError.js";
 // import { User } from  "../models/user.model.js";
 // import {uploadOnCloudinary} from "../utils/cloudinary.js";
@@ -78,7 +78,7 @@
 // })
 
 // export {registerUser}
-
+*/
 
 import { asyncHandler } from "../utils/asyncHandler.js";
 import {ApiError} from "../utils/ApiError.js"
