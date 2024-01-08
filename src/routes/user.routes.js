4985235dd1279250066c1e43e81189b5b56329fb
Kunal Kumar <kunal34255@gmail.com>
2024-01-08 09:20:46 +0530

    add comment, like.playlist,tweet models

--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -42,7 +42,7 @@ router
   .patch(verifyJWT, upload.single("avatar"), updateUserAvatar);
 router
   .route("/coverImage")
-  .patch(verifyJWT, upload.single("/coverImage"), updateUserCoverImage);
+  .patch(verifyJWT, upload.single("coverImage"), updateUserCoverImage);
 router.route("/c/:username").get(verifyJWT, getUserChannelProfile);
 router.route("/history").get(verifyJWT, getWatchHistory);
 
